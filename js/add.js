473e3a713ee38445eecdc563360d7cc1c231e8c8
happle joseph <happlejoseph66@gmail.com>
2026-04-24 02:40:59 +0530

    create note add and display and delete the note

---
+++ js/add.js
@@ -0,0 +1,34 @@
+let addBtn = document.getElementById('addBtn');
+let titleInput = document.getElementById('title');
+let contentInput = document.getElementById('content');
+
+let allNotes = JSON.parse(localStorage.getItem("notes")) || [];
+
+addBtn.addEventListener('click', ()=>{
+
+    let title = titleInput.value;
+    let content = contentInput.value;
+
+    if (title === "" || content === "") {
+        alert('Please add the notes!');
+        return;
+    }
+
+    let note = {
+        title : title,
+        content : content
+    }
+
+    allNotes.push(note);
+    localStorage.setItem("notes", JSON.stringify(allNotes));
+
+    titleInput.value = "";
+    contentInput.value = "";
+
+    alert('Note Added!');
+
+});
+
+function goToNotes () {
+    window.location.href = 'pages/display.html';
+}
---
+++ js/display.js
@@ -0,0 +1,32 @@
+
+let notesContainer = document.getElementById('notes');
+
+let allNotes = JSON.parse(localStorage.getItem("notes")) || [];
+
+function displayNotes () {
+    notesContainer.innerHTML = "";
+
+    allNotes.forEach((note, index)=> {
+        notesContainer.innerHTML +=
+        `
+        <div class="note">
+            <h3>${note.title}</h3>
+            <p>${note.content}</p>
+
+            <button class="edit-btn" onclick="editNote(${index})">Edit</button>
+            <button class="delete-btn" onclick="deleteNote(${index})">Delete</button>
+        </div>
+
+        `
+    });
+}
+
+function deleteNote (index) {
+    allNotes.splice(index, 1);
+    alert('you want to delete this note!');
+    localStorage.setItem("notes", JSON.stringify(allNotes));
+    alert('Deleted!');
+    displayNotes ();
+    
+}
+displayNotes ();
